95b21a924cd5035df67260e311192d5c927a1858
NovqiGarrix <novqigarrix@gmail.com>
2025-08-11 12:38:35 +0700

    Refactor Spendings component to fetch expenses from the database; remove hardcoded data and format amount display

--- components/Spendings.tsx
+++ components/Spendings.tsx
@@ -1,53 +1,12 @@
 import { Fonts } from "@/constants/Fonts";
+import { expenseCollection } from "@/lib/db";
+import Expense from "@/model/Expense.model";
+import { formatMoney } from "@/utils/formatter";
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { Q } from "@nozbe/watermelondb";
+import { withObservables } from "@nozbe/watermelondb/react";
 import { FlatList, Text, View } from "react-native";
 import { AddExpense } from "./AddExpense";
-
-
-const DATA = [
-    {
-        id: '1',
-        title: 'Rent',
-        amount: 1200,
-        date: new Date('2024-01-01T09:00:00').getTime()
-    },
-    {
-        id: '2',
-        title: 'Groceries',
-        amount: 300,
-        date: new Date('2024-01-03T14:30:00').getTime()
-    },
-    {
-        id: '3',
-        title: 'Utilities',
-        amount: 150,
-        date: new Date('2024-01-05T11:15:00').getTime()
-    },
-    {
-        id: '4',
-        title: 'Transportation',
-        amount: 100,
-        date: new Date('2024-01-07T08:45:00').getTime()
-    },
-    {
-        id: '5',
-        title: 'Internet',
-        amount: 80,
-        date: new Date('2024-01-10T16:20:00').getTime()
-    },
-    {
-        id: '6',
-        title: 'Phone Bill',
-        amount: 50,
-        date: new Date('2024-01-15T13:00:00').getTime()
-    },
-    {
-        id: '7',
-        title: 'Entertainment',
-        amount: 120,
-        date: new Date('2024-01-20T19:45:00').getTime()
-    }
-]
 
 function renderTimeAndDate(timestamp: number) {
     const date = new Date(timestamp);
@@ -63,7 +22,17 @@ function renderTimeAndDate(timestamp: number) {
     return <Text style={{ fontFamily: Fonts.ManropeRegular }}>{formattedTime} • {formattedDate}</Text>
 }
 
-export function Spendings() {
+export const Spendings = withObservables([], () => ({
+    expenses: expenseCollection.query(
+        Q.sortBy('created_at', Q.desc)
+    )
+}))(SpendingsComp) as React.ComponentType;
+
+interface SpendingsProps {
+    expenses: Expense[];
+}
+
+function SpendingsComp({ expenses }: SpendingsProps) {
 
     return (
         <View className="mt-6">
@@ -73,7 +42,7 @@ export function Spendings() {
             </View>
 
             <FlatList
-                data={DATA}
+                data={expenses}
                 showsVerticalScrollIndicator={false}
                 keyExtractor={item => item.id}
                 style={{ flexGrow: 0 }}
@@ -88,12 +57,12 @@ export function Spendings() {
 
                             <View>
                                 <Text style={{ fontFamily: Fonts.ManropeSemiBold }} className="text-dark-text text-xl ">{item.title}</Text>
-                                <Text style={{ fontFamily: Fonts.ManropeRegular }} className="text-dark-tabIconDefault text-base mt-1.5">{renderTimeAndDate(item.date)}</Text>
+                                <Text style={{ fontFamily: Fonts.ManropeRegular }} className="text-dark-tabIconDefault text-base mt-1.5">{renderTimeAndDate(item.createdAt)}</Text>
                             </View>
                         </View>
 
                         <Text style={{ fontFamily: Fonts.ManropeBold }} className="text-white text-2xl">
-                            ${item.amount}
+                            {formatMoney(item.amount)}
                         </Text>
                     </View>
                 )}
